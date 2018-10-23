a134b29bbe4f95f301c01f61ddd6060794b1cedf
Egor <zangedzu333gig@gmail.com>
2018-10-23 23:11:41 +0300

    Task 2

---
+++ console-logger.js
@@ -0,0 +1,22 @@
+const Logger = require('./logger');
+const moment = require('moment');
+
+class ConsoleLogger extends Logger{
+    constructor(prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
+        super(prefix, defaultLevel, dateFormat);
+    }
+
+    format(message, level = this.defaultLevel) {
+        return `${moment().format(this.dateFormat)} | ${this.prefix} | ${message}\n`;
+    }
+
+    log(message, level = this.defaultLevel) {
+        if (level === 'LOG') console.log(this.format(message, level));
+        else if if (level === 'INFO') console.info(this.format(message, level));
+        else if (level === 'WARN') console.warn(this.format(message, level));
+        else if (level === 'ERROR') console.error(this.format(message, level));
+        else console.log(this.format(message, this.defaultLevel));
+    }
+}
+
+module.exports = ConsoleLogger;
